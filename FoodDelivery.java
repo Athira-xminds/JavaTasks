abe42f5fbb536dbdce5c915ecbb5ea8667436bea
Athira-xminds <athirababuraj@xminds.com>
2026-07-10 10:10:53 +0530

    Add files via upload

--- FoodDelivery.java
+++ FoodDelivery.java
@@ -11,33 +11,33 @@ public class FoodDelivery {
 
 
         Runnable order101 = () -> {
-            System.out.println("Order-101 has started preparation.");
+            System.out.println("Order-101 has started preparation");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Order-101 is ready!");
+            System.out.println("Order-101 is ready");
         };
 
         Runnable order102 = () -> {
-            System.out.println("Order-102 has started preparation.");
+            System.out.println("Order-102 has started preparation");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Order-102 is ready!");
+            System.out.println("Order-102 is ready");
         };
 
         Runnable order103 = () -> {
-            System.out.println("Order-103 has started preparation.");
+            System.out.println("Order-103 has started preparation");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Order-103 is ready!");
+            System.out.println("Order-103 is ready");
         };
 
         Thread t1 = new Thread(order101);
